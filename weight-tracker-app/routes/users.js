0b10c687a6ad9cda4446229eea8d841c97497b5e
Anders Sundin <olsu2201@student.miun.se>
2023-11-21 13:49:05 +0100

    Lägger till bcrypt + hasning av lösenord

--- weight-tracker-app/routes/users.js
+++ weight-tracker-app/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
 const User = require('../Schemas/users');
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
@@ -7,27 +8,39 @@ router.get('/', async function (req, res, next) {
   res.status(200).json(users);
 });
 
-router.post('/', async function (req, res) {
+
+
+// ADD NEW USER
+
+router.post('/newUser', async function (req, res) {
 
   // save data to variabel from req to send to db.
-  const {name, email, password} = req.body;
+  const { name, email, password, startWeight, goalWeight } = req.body;
   try {
 
     // see if user already exists
-    const userExists = await User.findOne({email:email});
+    const userExists = await User.findOne({ email: email });
 
-    if (userExists){
-      return res.status(400).json({message: 'Eposten existerar redan i databasen'});
+    if (userExists) {
+      return res.status(400).json({ message: 'Eposten existerar redan i databasen' });
     }
-   
+    // Use bcrypt to hash password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+
     const newUser = await User.create({
       name: name,
       email: email,
-      password: password
+      password: hashedPassword,
+      startWeight: startWeight,
+      goalWeight: goalWeight
+
     });
     res.status(201).json(newUser)
   } catch (error) {
-      console.log('Något gick fel:' + error);
+    console.log('Något gick fel:' + error);
+    res.status(500);
   }
 });
 
