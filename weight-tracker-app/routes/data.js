352f7a16116e84fb0beeead53c9cf0287230d42a
Anders Sundin <olsu2201@student.miun.se>
2023-11-21 20:27:37 +0100

    skapar enpoints för att hämta och lagra mätdata

--- weight-tracker-app/routes/data.js
+++ weight-tracker-app/routes/data.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+//import schema
+const Measurement = require('../Schemas/measurement');
 /* DATA FOR LOGGING */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+//adding new measruement
+router.post('/', async function (req, res, next) {
+  const { userEmail, weight } = req.body;
+  
+  try{
+  const measurement = await Measurement.create({
+    weight: weight,
+    userEmail: userEmail
+  });
+
+  res.status(201).json(measurement);
+}
+catch (error) {
+console.log(error);
+return res.status(500).json({message: "Lagring misslyckades"});
+}
+});
+
+
+//FETCHING LAST 7 MEASUREMENTS FROM USER BY EMAIL
+
+router.post('/userSeven', async function (req, res) {
+  // Get seven posts by email, sort by date
+  const { userEmail } = req.body;
+try{
+  const measurements = await Measurement.find({userEmail}).sort({timestamp: -1}).limit(7);
+  res.status(200).json(measurements);
+}
+catch (error) {
+  console.log(error);
+  return res.status(500).json({message: "Hämtning misslyckades"});
+}
 });
 
 module.exports = router;
