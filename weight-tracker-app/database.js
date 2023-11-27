459ac98cdb6523d468d18cbde7beebce6adf3af7
Anders Sundin <olsu2201@student.miun.se>
2023-11-27 18:38:06 +0100

    Med env.variabler

--- weight-tracker-app/database.js
+++ weight-tracker-app/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 // Connect to the database with mongoose
-//mongoose.connect('mongodb://127.0.0.1:27017/WT'); 
-
+//mongoose.connect('mongodb://127.0.0.1:27017/WT');
+const dbPassword = process.env.DB_PASSWORD;
 // Connect to the database with mongoose
-mongoose.connect("mongodb+srv://anderssundin:C0ZaamQKRECzYOpK@cluster0.qxf9kh9.mongodb.net/WT?retryWrites=true&w=majority");
+mongoose.connect(`mongodb+srv://anderssundin:${dbPassword}@cluster0.qxf9kh9.mongodb.net/WT?retryWrites=true&w=majority`);
 
 
 
