e665e11c3a9b571003dc5751d70bfd7b30f5ec0c
Anders Sundin <olsu2201@student.miun.se>
2023-11-27 15:51:40 +0100

    testar lägga på render

--- weight-tracker-app/database.js
+++ weight-tracker-app/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
+const dbPassword = process.env.DB_PASSWORD;
 // Connect to the database with mongoose
-mongoose.connect('mongodb://127.0.0.1:27017/WT');
+mongoose.connect(`mongodb+srv://anderssundin:${dbPassword}@cluster0.qxf9kh9.mongodb.net/WT?retryWrites=true&w=majority`);
 
 
 // Handle errors and sucess0 with connection to database 
